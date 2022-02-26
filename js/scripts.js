2c9210f56b8c93e0716d0bc25cbc7b2f0339e0ed
Aaron Boyd <a.boyd91@yahoo.com>
2022-02-25 22:35:21 -0800

    add additional option for Pizza.prototype.price using a switch statement.

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,38 @@ function Pizza(toppings, size) {
 }
 const myPizza = new Pizza(["anchovies", "pineapple"], "medium");
 
-Pizza.prototype.price = function ()  {
-  let cost = 0;
-  for (let i = 0; i < this.toppings.length; i++)  {
-    cost += 2;
-  }
+// Pizza.prototype.price = function ()  {
+//   let cost = 0;
+//   for (let i = 0; i < this.toppings.length; i++)  {
+//     cost += 2;
+//   }
 
-  if (this.size === "small") {
-    cost += 9;
-  } else if (this.size === "medium") {
-    cost += 11;
-  } else if (this.size === "large") {
-    cost += 13;
+//   if (this.size === "small") {
+//     cost += 9;
+//   } else if (this.size === "medium") {
+//     cost += 11;
+//   } else if (this.size === "large") {
+//     cost += 13;
+//   }
+//   return cost;
+// }
+
+Pizza.prototype.price = function()  {
+  const toppingsQuantity = this.toppings.length;
+  let cost = toppingsQuantity * 2;
+
+  switch (this.size) {
+    case "small":
+      cost += 9;
+      break;
+    case "medium":
+      cost += 11;
+      break;
+    case "large":
+      cost += 13;
+      // break;
+    // default:
+    //   cost += 1;    
   }
   return cost;
-}
+};
